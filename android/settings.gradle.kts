9da40e8dd0af73c4259b447d07c57f15180a94d6
feifei00 <feifei00@feifei00deMacBook-Air-6.local>
2026-07-30 08:45:12 +0800

    use reliable Android dependency repositories

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,10 +11,6 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://repo.huaweicloud.com/repository/maven/")
         google()
         mavenCentral()
         gradlePluginPortal()
