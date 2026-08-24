2919d889665d7d83db8b9d74dae3c53e12aae82d
feifei00 <feifei00@feifei00deMacBook-Air-6.local>
2026-08-24 08:59:52 +0800

    fix: close production CI and four-client build gaps

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiException implements Exception {
   final int statusCode;
